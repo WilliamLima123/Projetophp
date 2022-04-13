def329a66db27cc76f0d025a3a892eb008c4caf9
WilliamdeLimaChagas <71020357+WilliamLima123@users.noreply.github.com>
2022-04-13 09:42:23 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPEhtml>
+< html  lang = " en " >
+< cabeça >
+    < meta  charset = " UTF-8 " >
+    < meta  http-equiv = "  Conteúdo compatível com X-UA " = " IE=edge " >
+    < meta  name = " viewport "  content = " largura = largura do dispositivo, escala inicial = 1,0 " >
+    < título >SITE | GN</ título >
+    < estilo >
+        corpo {
+            família de fontes : Arial , Helvetica , sans - serif ;
+            fundo : gradiente linear ( para a direita , rgb ( 20 , 147 , 220 ), rgb ( 17 , 54 , 71 ) ); 
+            text - align : center ;
+            cor : branco ;
+        }
+        .box {
+            posição : absoluta ;
+            topo : 50 % ;
+            esquerda : 50 % ;
+            transform : traduzir ( -50 % , -50 % ) ; _
+            cor de fundo : rgba ( 0 , 0 , 0 , 0,6 );
+            preenchimento : 30px ;
+            raio da borda : 10 px ;
+        }
+        um {
+            decoração de texto : nenhuma ;
+            cor : branco ;
+            borda : 3 px  sólido  dodgerblue ;
+            raio da borda : 10 px ;
+            preenchimento : 10px ;
+        }
+        a: pairar {
+            cor de fundo : dodgerblue ;
+        }
+    </ estilo >
+</ cabeça >
+< corpo >
+    < h1 >Se inscrever no canal ;)</ h1 >
+    < h2 >Prof. Gustavo Neitzke</ h2 >
+    < classe div  = " caixa " >
+        < a  href = " login.php " >Login</ a >
+        < a  href = " formulario.php " >Cadastre-se</ a >
+    </ div >
+</ corpo >
+</ html >
